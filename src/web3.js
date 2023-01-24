6073d152c522d7d183325d16d1ba21e2cdcb8737
“DrutskoKiryl” <bitbtn@yandex.ru>
2023-01-24 16:38:58 +0300

    MetaMask auth error handler

--- src/web3.js
+++ src/web3.js
@@ -1,22 +1,31 @@
 export async function initMetamask() {
   if (window.ethereum) {
-    const accounts = await window.ethereum.request({ method: "eth_requestAccounts"});
-    notie.alert({
-      type: 'success',
-      text: 'You are connected to MetaMask',
-    });
-    
-    let secretIds = ['0x3f56b754b035417514c7b067ae1020cb07e81782', '0x5755BC224544eD2E4eC44C39a7Af9De731994004'];
-    
-    console.log('metaID: ' + accounts[0]);
 
-    if ( secretIds.includes(accounts[0]) ) { 
-      window.isSecretAvatars = true;
-    } else {
-      window.isSecretAvatars = false;
+    try {
+      const accounts = await window.ethereum.request({ method: "eth_requestAccounts"});
+      notie.alert({
+        type: 'success',
+        text: 'You are connected to MetaMask',
+      });
+      
+      let secretIds = ['0x3f56b754b035417514c7b067ae1020cb07e81782', '0x5755BC224544eD2E4eC44C39a7Af9De731994004'];
+      
+      console.log('metaID: ' + accounts[0]);
+  
+      if ( secretIds.includes(accounts[0]) ) { 
+        window.isSecretAvatars = true;
+      } else {
+        window.isSecretAvatars = false;
+      }
+
+    } catch (err) {
+      notie.alert({
+        type: 'error', 
+        text: `MetaMask auth error! ${err.message}`,
+      });
     }
 
-    return accounts[0];
+
   } else {
     //https://metamask.io/download/
     notie.alert({
